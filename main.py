84e89ee2ba3835bdc12c23e4b78aa94d250acad2
tobkirch <159419620+tobkirch@users.noreply.github.com>
2024-02-08 17:44:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -72,9 +72,6 @@ def main():
                 repo.update_file(contents.path, "Daten aktualisiert", updated_df.to_csv(index=False), contents.sha)
                 st.success("Daten erfolgreich gespeichert!")
                 st.session_state['prediction'] = None
-
-                st.header("Klicke hier um mit einem neuen Bild zu starten:")
-                if st.button("Neustart"):
                     
 
 
