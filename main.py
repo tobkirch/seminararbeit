1b7c75bc1fc37b550996ffc1f9114db210594926
tobkirch <159419620+tobkirch@users.noreply.github.com>
2024-02-09 11:26:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main():
             uploaded_image = st.file_uploader('Lade das Bild einer Wendeschneidplatte hoch', type=['jpg', 'jpeg', 'png'])
         with t2:
             # Bild aufnehmen
-            uploaded_image = st.camera_input(label_visibility="hidden")
+            uploaded_image = st.camera_input(label, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
         
         if uploaded_image is not None:
             # Bild zuschneiden
