91c53aa32c2635301bd814b28fdba0a9e6ad4fae
tobkirch <159419620+tobkirch@users.noreply.github.com>
2024-03-12 22:13:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -129,20 +129,21 @@ def main():
         t3, t4 = st.tabs(["Tabelle", "Diagramm"])
         with t3:
             # Suchfunktion hinzufügen
-            st.subheader("Suche")
             col1, col2 = st.columns(2)
             with col1:
-                search_column = st.selectbox("In welcher Spalte suchen?", df.columns)
+                search_column = st.selectbox("In Spalte suchen?", df.columns)
             with col2:
                search_query = st.text_input("Suchwort eingeben")
-            col3, col4 = st.columns(2)
+            col3, col4 = st.columns(4)
             with col3:
                 search_button = st.button("Suche")
             with col4:
                 showAll_button = st.button("Alles anzeigen")
             if search_button:
-                df = df[df[search_column].str.contains(search_query, case=False)]
-            st.write(df)
+                df_show = df[df[search_column].str.contains(search_query, case=False)]
+            if showAll_button:
+                df_show = df
+            st.write(df_show)
 
         
         with t4:
