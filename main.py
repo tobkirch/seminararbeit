ba29cd7e33f1264e1a664b3fe244cf0afe25bf90
tobkirch <159419620+tobkirch@users.noreply.github.com>
2024-03-13 17:08:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -145,6 +145,7 @@ def main():
                 st.write('<div style="height: 28px;"></div>', unsafe_allow_html=True)
                 search_button = st.button("Tabelle filtern")
             with col_ShowAllButton:
+                st.write('<div style="height: 28px;"></div>', unsafe_allow_html=True)
                 showAll_button = st.button("Alles anzeigen")
             if search_button:
                 df_show = df[df[search_column].str.contains(search_query, case=False)]
