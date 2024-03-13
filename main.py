e6c02f3d7a414b5a49b68dbfc180d9b6bbe29952
tobkirch <159419620+tobkirch@users.noreply.github.com>
2024-03-13 17:07:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,11 @@ github_repo_name = "seminararbeit"
 github_repo_name2 = "test"
 github_file_path = "ergebnisse.csv"
 
+#Variablen
 if 'prediction' not in st.session_state:
     st.session_state['prediction'] = None
-
 if 'show' not in st.session_state:
     st.session_state['show'] = True
-
 if 'saved' not in st.session_state:
     st.session_state['saved'] = False
 
@@ -28,26 +27,26 @@ if 'saved' not in st.session_state:
 if 'model' not in st.session_state:
     model_path = "mnv2_model"  # Modell im selben Ordner wie main.py
     st.session_state['model'] = tf.keras.models.load_model(model_path)
-    
-st.title('Bildklassifizierung Werkzeugverschleiß')
-tab1, tab2 = st.tabs(["Vorhersage tätigen", "Gespeicherte Daten"])
 
 # Streamlit-Anwendung
+st.title("Bildklassifizierung Werkzeugverschleiß")
+tab_Prediction, tab_Data = st.tabs(["Vorhersage tätigen", "Gespeicherte Daten"])
 def main():
-    with tab1:   
+    with tab_Prediction:
+        # Bild hochladen
         st.header("Schritt 1: Bild auswählen")
         st.write("Wähle das Bild aus für das eine Vorhersage getätigt werden soll. Hierfür bestehen zwei Möglichkeiten:")
-        # Bild hochladen
-        tab3, tab4 = st.tabs(["Bild hochladen", "Bild aufnehmen"])
-        with tab3:
+        tab_Upload, tab_Camera = st.tabs(["Bild hochladen", "Bild aufnehmen"])
+        with tab_Upload:
             st.write("Lade das Bild einer Wendeschneidplatte hoch")
             uploaded_image = st.file_uploader("Wenn du bereits ein Bild mit der Kamera aufgenommen hast, wird es hierduch ersetzt", type=['jpg', 'jpeg', 'png'])
-        with tab4:
+        with tab_Camera:
             if uploaded_image is None:
                 camera_image = st.camera_input(" ")
             else:
                 st.info("Entferne erst das hochgeladene Bild, bevor du hier eines mit deiner Kamera aufnehmen kannst")
-        
+                
+        #Nach dem Hocladen
         if uploaded_image is not None or camera_image is not None:
             # Bild zuschneiden
             st.divider()
@@ -56,37 +55,36 @@ def main():
                 image = Image.open(uploaded_image)
             else:
                 image = Image.open(camera_image)
-            
             st.write("Schneide das Bild auf die obere Schneidecke und -kante zu, indem du die Regler verschiebst.")
-            c1, c2 = st.columns([30,70], gap="medium")
-            with c1:
+            col_Slider, col_Image = st.columns([30,70], gap="medium")
+            with col_Slider:
                 image = crop_image(image)
-            with c2:
+            with col_Image:
                 st.image(image, caption="Zugeschnittenes Bild", use_column_width=True)
             
-            # Button zum Vorhersagen
+            # Vorhersage tätigen
             st.divider()
-            st.header('Schritt 3: Vorhersage tätigen')
+            st.header("Schritt 3: Vorhersage tätigen")
             st.write("Klicke hier um eine Vorhersage für das ausgewählte Bild zu tätigen:")
-            c3, c4= st.columns([30, 70])
-            with c3:
-                if st.button('Vorhersage tätigen'):
+            col_PredictButton, col_Prediction= st.columns([30, 70])
+            with col_PredictButton:
+                if st.button("Vorhersage tätigen"):
                         # Vorhersage mit dem Modell
                         st.session_state['prediction'] = predict(image)
-            with c4:
+            with col_Prediction:
                 if st.session_state.prediction is None:
-                    st.info('Vorhersage des Modells: ...')
+                    st.info("Vorhersage des Modells: ...")
                 elif st.session_state.prediction == "Defekt":
-                    st.error('Defekt')
+                    st.error("Defekt")
                 elif st.session_state.prediction == "Mittel":
-                    st.warning('Mittel')
+                    st.warning("Mittel")
                 elif st.session_state.prediction == "Neuwertig":
-                    st.success('Neuwertig')
+                    st.success("Neuwertig")
     
-            # Zusätzliche Bauteildaten
+            # Vorhersage speichern
             if st.session_state.prediction is not None:
                 st.divider()
-                st.header('Schritt 4: Vorhersage speichern')
+                st.header("Schritt 4: Vorhersage speichern")
                 if st.session_state.show is True:
                     # Textfeldeingaben
                     st.write("Gib zusätzliche Daten über die Wendeschneidplatte an um sie mit der Vorhersage zu speichern")
@@ -100,14 +98,13 @@ def main():
                     
                     # Speichern Button
                     if st.button("Daten Speichern"):
-    
                         # Laden der bisherigen Daten von GitHub
                         g = Github(github_token)
                         repo = g.get_repo(f"{github_repo_owner}/{github_repo_name}")
                         contents = repo.get_contents(github_file_path)
                         csv_content = contents.decoded_content.decode('utf-8')
                         existing_df = pd.read_csv(StringIO(csv_content))
-                        
+                        #Neuen DataFrame erstellen
                         new_data = {"Name des Bauteils": [bauteil_name], "Werkzeugtyp": [werkzeugtyp], "Vorschub": [vorschub], "Drehzahl": [drehzahl], "Zustellung": [zustellung], "Bearbeitungsdauer": [bearbeitungsdauer], "Vorhersage": [st.session_state.prediction]}
                         new_df = pd.DataFrame(new_data)
                         updated_df = pd.concat([existing_df, new_df], ignore_index=True)
@@ -123,28 +120,31 @@ def main():
             st.session_state.show = True
             st.session_state.saved = False
 
-    with tab2:
+    with tab_Data:
         st.header("Deine gespeicherten Daten")
+        #Laden der Daten von Github
         g = Github(github_token)
         repo = g.get_repo(f"{github_repo_owner}/{github_repo_name}")
         contents = repo.get_contents(github_file_path)
         csv_content = contents.decoded_content.decode('utf-8')
         df = pd.read_csv(StringIO(csv_content))
         df_show = df
-        tab5, tab6 = st.tabs(["Tabelle", "Diagramm"])
-        with tab5:
+        
+        #Anzeigen der Daten
+        tab_Table, tab_Diagramm = st.tabs(["Tabelle", "Diagramm"])
+        with tab_Table:
             st.write("Hier werden deine gespeicherten Daten als Tabelle angezeigt.")
             st.write("Über die Filterung lässt sich steuern welche Einträge angezeigt werden sollen.")
             # Filterfunktion hinzufügen
-            col1, col2, col3, col4 = st.columns(4)
-            with col1:
+            col_Column, col_Query, col_FilterButton, col_ShowAllButton = st.columns(4)
+            with col_Column:
                 search_column = st.selectbox("Spalte", df.columns)
-            with col2:
+            with col_Query:
                 search_query = st.text_input("Suchwort")
-            with col3:
+            with col_FilterButton:
                 st.write('<div style="height: 28px;"></div>', unsafe_allow_html=True)
                 search_button = st.button("Tabelle filtern")
-            with col4:
+            with col_ShowAllButton:
                 showAll_button = st.button("Alles anzeigen")
             if search_button:
                 df_show = df[df[search_column].str.contains(search_query, case=False)]
@@ -153,40 +153,33 @@ def main():
             if showAll_button:
                 df_show = df
             st.write(df_show)
-        
-        with tab6:
+        with tab_Diagramm:
             # Verschleißverlauf über die Zeit anzeigen
             st.write("Hier werden deine gespeicherten Daten aus Diagramm angezeigt, dass die Vorhersage des Verschleißgrades über die Bearbeitungszeit aufführt.")
             if not df.empty:
                 df["Bearbeitungsdauer"] = pd.to_numeric(df["Bearbeitungsdauer"], errors='coerce')
                 df["Vorhersage"] = df["Vorhersage"].astype('category')
-            
-                # Dropdown-Liste für die Auswahl der Bauteile hinzufügen
+                # Dropdown-Liste für die Auswahl der Bauteile
                 selected_parts = st.multiselect("Anzuzeigende Bauteile auswählen", df["Name des Bauteils"].unique(), default=df["Name des Bauteils"].unique())
-            
                 # Erstellen des Diagramms mit Matplotlib oder Seaborn
                 fig, ax = plt.subplots(figsize=(10, 3))
-            
                 # Gruppierung nach Bauteilnamen und Erstellung von Plots für ausgewählte Gruppen
                 grouped = df.groupby("Name des Bauteils")
                 for name, group in grouped:
                     if name in selected_parts:
                         chart_data = group[["Bearbeitungsdauer", "Vorhersage"]]
-                        chart_data = chart_data.sort_values(by="Bearbeitungsdauer")  # Sortierung nach Bearbeitungsdauer
-            
+                        # Sortierung nach Bearbeitungsdauer
+                        chart_data = chart_data.sort_values(by="Bearbeitungsdauer")
                         # Plotten des Verschleißverlaufs für ausgewählte Bauteile
                         sns.lineplot(data=chart_data, x="Bearbeitungsdauer", y="Vorhersage", ax=ax, label=name)
-            
                 # Hilfslinien hinzufügen
                 ax.grid(True, which='both', linestyle='--', linewidth=0.5)
-            
                 # Achsenbeschriftungen und Titel hinzufügen
                 ax.set_title("Verschleißverlauf über die Zeit")
                 ax.set_xlabel("Bearbeitungsdauer")
                 ax.set_ylabel("Vorhersage")
                 ax.tick_params(axis='x')
                 ax.legend(title="Bauteil")
-            
                 # Diagramm anzeigen
                 st.pyplot(fig)
     
@@ -196,10 +189,8 @@ def crop_image (image):
     top = st.slider("Oberer Rand:", 0, image.height, 0)
     right = st.slider("Rechter Rand:", 0, image.width, image.width)
     bottom = st.slider("Unterer Rand:", 0, image.height, image.height)
-    
     # Bild zuschneiden
     cropped_image = image.crop((left, top, right, bottom))
-    
     return cropped_image
     
 def predict(img):
@@ -207,16 +198,12 @@ def predict(img):
     # Laden des Bildes und Umwandeln in das richtige Format
     img = img.resize((224, 224))  # Skalieren des Bildes auf die gewünschte Größe
     img = np.array(img) / 255.0  # Normalisieren des Bildes
-    
     # Hinzufügen einer zusätzlichen Dimension, um eine Batch-Dimension zu simulieren
     img = np.expand_dims(img, axis=0)
-    
     # Vorhersage durchführen
     predictions = st.session_state.model.predict(img)
-    
     # Extrahieren der wahrscheinlichsten Klasse
     predicted_class = np.argmax(predictions[0])
-    
     # Rückgabe der vorhergesagten Klasse
     return class_names[predicted_class]
     
